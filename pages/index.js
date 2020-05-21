01625bfaa8dd0662f61efa78f1170a41eafb7ae0
Bach <bly@cochlear.com>
2020-05-21 12:48:15 +1000

    Fetch and display posts on Home page

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,33 @@ import Head from "next/head";
 import { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
 
+import Link from "next/link";
+
+const importPosts = async () => {
+  // https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
+  // second flag in require.context function is if subdirectories should be searched
+  const markdownFiles = require
+    .context("../content/posts", false, /\.md$/)
+    .keys()
+    .map((relativePath) => relativePath.substring(2));
+  return Promise.all(
+    markdownFiles.map(async (path) => {
+      const markdown = await import(`../content/posts/${path}`);
+      return { ...markdown, slug: path.substring(0, path.length - 3) };
+    })
+  );
+};
+
 export default class Home extends Component {
+  static async getInitialProps() {
+    const postsList = await importPosts();
+
+    return { postsList };
+  }
+
   render() {
     let { title, cats } = attributes;
+    const { postsList } = this.props;
     return (
       <>
         <Head>
@@ -22,6 +46,28 @@ export default class Home extends Component {
               </li>
             ))}
           </ul>
+          <div className="blog-list">
+            {postsList.map((post) => {
+              return (
+                <Link href={`blog/post/${post.slug}`}>
+                  <a>
+                    <img src={post.attributes.thumbnail} />
+                    <h2>{post.attributes.title}</h2>
+                  </a>
+                </Link>
+              );
+            })}
+            <style jsx>{`
+              .blog-list a {
+                display: block;
+                text-align: center;
+              }
+              .blog-list img {
+                max-width: 100%;
+                max-height: 300px;
+              }
+            `}</style>
+          </div>
         </article>
       </>
     );
